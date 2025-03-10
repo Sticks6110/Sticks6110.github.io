72ee038a7109a264191442c712f91d5c533ba0e0
Sticks <59712082+Sticks6110@users.noreply.github.com>
2025-03-09 21:19:22 -0400

    3D Sudoku

---
+++ rust-wasm/sudoku-wasm/pkg/sudoku_wasm_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const generate_sudoku: (a: number) => any;
+export const get_potential: (a: number, b: number, c: number, d: number, e: number, f: number) => any;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_start: () => void;
